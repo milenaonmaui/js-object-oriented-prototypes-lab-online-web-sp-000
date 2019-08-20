c2477147722a110b8c1a044d4bf6d6eb5f6ac2ea
Milena Kari <milenakari@yahoo.com>
2019-08-20 21:24:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function BoardMember(name, homeState, training){
   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
+   this.homeState = homeState
+   this.training = training
+}
+BoardMember.prototype.veto = function(){
+     return 'No, I must disagree'
+}
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }
